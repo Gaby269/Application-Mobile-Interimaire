98d8aad9e321f78299f625e6129806ca6f601a77
Gatien ACER <le.magicien.annonyme@gmail.com>
2022-11-04 07:48:38 +0100

    Generation de CSP

--- HAI710I - Fondement de l'IA/Projet_CSP/generation/generation_densite.sh
+++ HAI710I - Fondement de l'IA/Projet_CSP/generation/generation_densite.sh
@@ -2,6 +2,6 @@ for dens in 149 297 446 535
 do
     for ((i=211; i>178; i-=3))
     do
-        ./urbcsp 35 17 $dens $i 10 > "reseaux/bench$dens/csp_$i.txt"
+        ./urbcsp 35 17 $dens $i 10 > "../reseaux/bench$dens/csp_$i.txt"
     done
 done
