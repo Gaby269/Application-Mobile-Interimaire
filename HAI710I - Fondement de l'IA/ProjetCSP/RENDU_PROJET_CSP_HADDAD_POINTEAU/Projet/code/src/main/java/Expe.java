96e332461f379475f40907579ff0f4aa4b4ce1a9
Leogendra <le.magicien.annonyme@gmail.com>
2022-11-10 17:46:42 +0100

    fd

--- HAI710I - Fondement de l'IA/ProjetCSP/RENDU_PROJET_CSP_HADDAD_POINTEAU/Projet/code/src/main/java/Expe.java
+++ HAI710I - Fondement de l'IA/ProjetCSP/RENDU_PROJET_CSP_HADDAD_POINTEAU/Projet/code/src/main/java/Expe.java
@@ -9,10 +9,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 // time
-// import java.lang.management.ManagementFactory;
-// import java.lang.management.ThreadMXBean;
-// import java.util.Scanner; // Scanner pour les entrées
-// import java.time.LocalTime; // Current time
+ import java.util.Scanner; // Scanner pour les entrées
 
 public class Expe {
 
@@ -45,14 +42,12 @@ public class Expe {
 		int nbRes = 10;								// nombre de reseau fixer pcq pas pu recup
 		int tailleDom = 17;							// on fixe le domaine pour ne pas ouvrir le fichier
 	    FileWriter fichier_resultats; 				// fichier qu'on va ecrire les reusltat
-	    String bench = "bench2";					//je recupere le nom du fichier
-	    System.out.println(bench);
 	    
-	    /*
 		//Donner le nom du fichier
-		System.out.println("Veuillez entrée le nom du benchmark dans le dossier /reseaux : ");	//je demande a ce qu'on entre une solution
+		System.out.println("Veuillez entrer le nom du benchmark dans le dossier /reseaux :");	//je demande a ce qu'on entre une solution
 		Scanner clavier = new Scanner(System.in);												//creation du scanneur
-	    */
+		String bench = clavier.nextLine();
+		clavier.close();
 		
 	    fichier_resultats = new FileWriter("../resultats/result_"+bench+".csv",false); 	// écriture dans le fichier .CSV en ecrasant le fichier d'avant
 		fichier_resultats.write("Durete;% solutions;temps moyen (s)\n"); 				// en-tête du fichier
@@ -60,7 +55,6 @@ public class Expe {
 		// parsing des réseaux
 		String path = "../reseaux/"+bench+"/";			// ouverture du fichier
 		File reseaux = new File(path); 					// fichiers des reseaux
-		System.out.println(reseaux);
 		File listeFichiers[] = reseaux.listFiles(); 	// liste des fichiers
 		int nbDiff = listeFichiers.length; 				// nb de fichiers
 		
@@ -78,43 +72,40 @@ public class Expe {
 			for(int nb=1 ; nb<=nbRes; nb++) { 			// pour chaque reseau
 				Model model=lireReseau(readFile); 		// création du modèle
 				Solver solver = model.getSolver(); 		// initialisation du solver
-				solver.limitTime("10s"); 				// set du Time Out (TO) à 30s
+				solver.limitTime("30s"); 		// set du Time Out (TO) à 30s
 				
-				System.out.println("Résolution du réseau "+nb); 	// indication visuelle de l'avancée du benchmark
+				System.out.println("Résolution du réseau n°"+nb); 	// indication visuelle de l'avancée du benchmark
 				long startTime = System.nanoTime();					// calcul de lheure de départ
 
 				if (solver.solve()) { 							 	// si le modèle à au moins une solution
 					tempsMoyen += System.nanoTime() - startTime; 	// calcul du temps d'exécution
-					System.out.println("Solution trouvée\n");
+					System.out.println("-> Solution trouvée :)\n");
 					nbSoluce++;										// augmente le nombre de reseau qui a au moins une solution
 				}
 				else if (solver.isStopCriterionMet()){				// si le solver time out
-					System.out.println("Time out !\n");				// affichage 
+					System.out.println("-> Time out !\n");		// affichage 
 					nbTO++; 										// incrémentation du compteur de Timeouts
-					//on a pas besoin d'ajouter 
+					//on n'ajoute pas le temps moyen car  on ne prend pas en compte les soler qui ont TO
 				}
 				else {												// sinon pas de solution donc pas d'affichage
 					tempsMoyen += System.nanoTime() - startTime; 	// calcul du temps d'exécution
-					System.out.println("Pas de solution\n");		// affichage
+					System.out.println("-> Pas de solution :(\n");// affichage
 				}
 			}//fin reseau
 			
 			//ECRITURE DANS LE FICHIER
 				//nombre de tuples
-			int nbTuples = Integer.parseInt((fic.split("\\.")[0]).split("\\_")[1]);				//recuperation du nb de tuples autorisés dans le titre du fichiers
+		int nbTuples = Integer.parseInt((fic.split("\\.")[0]).split("\\_")[1]);											 //recuperation du nb de tuples autorisés dans le titre du fichiers
 				//Dureté
-			double durete = (double) (tailleDom*tailleDom - nbTuples) / (tailleDom*tailleDom);		//dureté donnée dant le fichier (d²-t)/d²
-			durete = (double) Math.round(durete * 100) / 100;
+			double durete = (double)Math.round(100 * (double)(tailleDom*tailleDom - nbTuples) / (tailleDom*tailleDom)) / 100;			//dureté donnée dant le fichier (d²-t)/d²
 				//Pourcentage
-			double pourcentage = (nbTO==nbRes)? -1 : (100*nbSoluce/(nbRes-nbTO));					//calcul du pourcentage * si tout le monde a time out alors pas de pourcentage (-1) * sinon on recalcule le nombre de reseau qui ont pas timeout
-			pourcentage = Math.round(pourcentage);
+		double pourcentage = (nbTO==nbRes)? -1 : Math.round(100*nbSoluce/(nbRes-nbTO));													//calcul du pourcentage * si tout le monde a time out alors pas de pourcentage (-1) * sinon on recalcule le nombre de reseau qui ont pas timeout, on arondi à deux chiffres après la virgule
 				//Temps moyen
-			double tempsMoy = (nbTO==nbRes)? -1 : ((double)tempsMoyen/(nbRes-nbTO))/1000000000;		//calcul du temps moyen de trouver une solution ou non en seconde
-			tempsMoy = (double) Math.round(tempsMoy * 1000) / 1000;
+			double tempsMoy = (nbTO==nbRes)? -1 : (double)Math.round(1000 * ((double)tempsMoyen/(nbRes-nbTO)) / 1000000000) / 1000;		//calcul du temps moyen de trouver une solution ou non en seconde, on arondi à trois chiffres après la virgule
 				//Ecriture
-			fichier_resultats.write(durete + ";" + pourcentage + "%;" + tempsMoy + "\n");			//ecriture dans le fichier
+			fichier_resultats.write(durete + ";" + pourcentage + "%;" + tempsMoy + "\n");												//ecriture dans le fichier
 
-			//après on pourra tracer la courbe dans excel pour obtenir la courbe du pourcentage de solution en fonction de la dureté et le temps moyen de trouver ou non des solutions
+			// après on pourra tracer la courbe dans Excel pour obtenir la courbe du pourcentage de solution en fonction de la dureté et le temps moyen pour trouver s'il y a ou non une solutions
 			
 		} // fin du parsing de tous les fichiers
 		
