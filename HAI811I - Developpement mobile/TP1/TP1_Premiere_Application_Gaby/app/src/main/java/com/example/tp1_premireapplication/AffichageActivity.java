cd813c8f598ac614348450ce5177385f4751fbc6
Gabrielle P <114810027+Gaby269@users.noreply.github.com>
2023-02-06 00:56:47 +0100

    [GP] Ajout de mon actuel TP1 si on doit le rendre

---
+++ HAI811I - Developpement mobile/TP1/TP1_Premiere_Application_Gaby/app/src/main/java/com/example/tp1_premireapplication/AffichageActivity.java
@@ -0,0 +1,73 @@
+package com.example.tp1_premireapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+
+public class AffichageActivity extends AppCompatActivity {
+
+    ///////////////////////////////
+    // AFFICHAGE DES COORDONN2ES //
+    ///////////////////////////////
+
+    @SuppressLint({"ResourceType", "SetTextI18n", "MissingInflatedId"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affichage);
+
+        // Récuperer les données saisies
+        Intent intent = getIntent();
+
+        // Récuperation des id des TextView
+        TextView nom_affichage = findViewById(R.id.nom_affichage);
+        TextView prenom_affichage = findViewById(R.id.prenom_affichage);
+        TextView age_affichage = findViewById(R.id.age_affichage);
+        TextView tel_affichage = findViewById(R.id.tel_affichage);
+        TextView comp_affichage = findViewById(R.id.comp_affichage);
+
+        // Modifier le texte à partir des Extra
+        nom_affichage.setText(intent.getStringExtra("nomAff"));
+        prenom_affichage.setText(intent.getStringExtra("prenomAff"));
+        age_affichage.setText(intent.getStringExtra("ageAff"));
+        tel_affichage.setText(intent.getStringExtra("telAff"));
+        comp_affichage.setText(intent.getStringExtra("compAff"));
+
+
+        // Bouton pour retourner à la page d'acueil
+        Button buttonRetour = findViewById(R.id.retour_bouton);
+        buttonRetour.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Réafficher la saisie des données au départ
+                Intent i = new Intent(AffichageActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+
+        });
+
+        // Bouton pour dire okk
+        Button buttonOk = findViewById(R.id.ok_button);
+        buttonOk.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Réafficher la saisie des données au départ
+                Intent i = new Intent(AffichageActivity.this, AppelActivity.class);
+                TextView tel_affichage = findViewById(R.id.tel_affichage);
+                i.putExtra("telAff", tel_affichage.getText().toString());
+                startActivity(i);
+            }
+
+        });
+    }
+
+}
---
+++ HAI811I - Developpement mobile/TP1/TP1_Premiere_Application_Gaby/app/src/main/java/com/example/tp1_premireapplication/AppelActivity.java
@@ -0,0 +1,81 @@
+package com.example.tp1_premireapplication;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
+import android.telecom.Call;
+
+import java.text.BreakIterator;
+
+public class AppelActivity extends AppCompatActivity {
+
+    //////////////////////////////////
+    // AFFICHAGE DE FINI DE VALIDER //
+    //////////////////////////////////
+
+    @SuppressLint({"ResourceType", "SetTextI18n", "MissingInflatedId"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appel);
+
+        // Récuperer les données saisies
+        Intent i = getIntent();
+        // Récuperation des id des TextView pour l'appel
+        TextView affTel = findViewById(R.id.affichageTel);
+        // Modifier le texte à partir des Extra pour l'appel
+        affTel.setText(i.getStringExtra("telAff"));
+
+
+        // Bouton pour retourner à la page d'acueil
+        Button buttonAppel = findViewById(R.id.appel_button);
+        buttonAppel.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("tel:" + affTel.getText().toString());
+                Intent i_appel = new Intent(Intent.ACTION_CALL,uri);
+                if (ActivityCompat.checkSelfPermission(AppelActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    String [] permissions = {
+                            Manifest.permission.CALL_PHONE
+                    };
+                    requestPermissions(permissions,1000);
+                    return;
+                }
+                startActivity(i_appel);
+            }
+
+        });
+
+
+        // Bouton pour retourner à la page d'acueil
+        Button buttonRetour = findViewById(R.id.retour_bouton);
+        buttonRetour.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //Réafficher la saisie des données au départ
+                Intent i = new Intent(AppelActivity.this, MainActivity.class);
+                startActivity(i);
+
+            }
+
+        });
+    }
+
+}
