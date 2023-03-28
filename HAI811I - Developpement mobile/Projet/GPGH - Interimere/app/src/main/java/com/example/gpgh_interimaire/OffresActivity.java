d17c14019cfb9a592e6513865266a9026140ce7a
Gatien ACER <le.magicien.annonyme@gmail.com>
2023-03-28 11:10:28 +0200

    [Dev Mobile] : Refactor

---
+++ HAI811I - Developpement mobile/Projet/GPGH - Interimere/app/src/main/java/com/example/gpgh_interimaire/OffresActivity.java
@@ -0,0 +1,27 @@
+package com.example.gpgh_interimaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class OffresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button inscriptionButton = findViewById(R.id.boutton_inscription);
+        inscriptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(OffresActivity.this, InscriptionActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ HAI811I - Developpement mobile/Projet/GPGH - Interimere/app/src/main/java/com/example/gpgh_interimaire/RecapPaiementActivity.java
@@ -0,0 +1,29 @@
+package com.example.gpgh_interimaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RecapPaiementActivity extends AppCompatActivity {
+
+    @Override
+    @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recap_paiement);
+
+
+        Button inscriptionButton = findViewById(R.id.boutton_confirmer);
+        inscriptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RecapPaiementActivity.this, MoyenPaiementActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
