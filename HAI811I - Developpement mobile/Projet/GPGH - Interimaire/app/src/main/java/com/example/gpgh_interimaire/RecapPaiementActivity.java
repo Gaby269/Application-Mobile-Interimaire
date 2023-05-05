6cc5882454759be9ff0f50aa040558a80bc321f5
Gabrielle P <114810027+Gaby269@users.noreply.github.com>
2023-05-05 12:21:13 +0200

    [GP][HAI811I] Ajout du visuel pour Recap paiement

--- HAI811I - Developpement mobile/Projet/GPGH - Interimaire/app/src/main/java/com/example/gpgh_interimaire/RecapPaiementActivity.java
+++ HAI811I - Developpement mobile/Projet/GPGH - Interimaire/app/src/main/java/com/example/gpgh_interimaire/RecapPaiementActivity.java
@@ -7,15 +7,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RecapPaiementActivity extends AppCompatActivity {
 
-    TextView typeAboTextView, prixTextView;
+    TextView typeAboTextView, prixTextView, totalTextView;
     String typeAbo;
 
     @Override
-    @SuppressLint({"MissingInflatedId", "LocalSuppress", "SetTextI18n"})
+    @SuppressLint({"MissingInflatedId", "LocalSuppress", "SetTextI18n", "WrongViewCast"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recap_paiement);
@@ -27,9 +29,32 @@ public class RecapPaiementActivity extends AppCompatActivity {
 
         //modif du texte du paiment de la typeAboTextView et prixTextView
         typeAboTextView = findViewById(R.id.typeAboTextView);
-        typeAboTextView.setText("Abonnement " + typeAbo + " : ");
+        if (typeAbo.contains("Ponctuel")){
+            typeAboTextView.setText("Abonnement Ponctuel : ");
+        }
+        else{
+            typeAboTextView.setText("Abonnement " + typeAbo + " : ");
+        }
         prixTextView = findViewById(R.id.prixTextView);
         prixTextView.setText(prix+" €");
+        totalTextView = findViewById(R.id.totalTextView);
+        totalTextView.setText("Total : "+prix+" €");
+
+
+        ImageView boutonCodePromo = findViewById(R.id.bouton_codePromo);
+        EditText codePromo = findViewById(R.id.codePromo);
+        boutonCodePromo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (codePromo.length() == 0){
+                    codePromo.setError("Veuillez remplir le champ");
+                }
+                // Ajouter au prix le code promo
+                // le fait que ya - quelque chose
+                // reclacul du total
+            }
+        });
+
 
         Button inscriptionButton = findViewById(R.id.boutton_confirmer);
         inscriptionButton.setOnClickListener(new View.OnClickListener() {
