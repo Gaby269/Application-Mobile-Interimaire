7d08aa91f45cd2464abcc6d645a21ec53708e4de
Gabrielle P <114810027+Gaby269@users.noreply.github.com>
2023-05-05 22:54:34 +0200

    [HAI811I][GP] Ajout du visuel et de code promo pour recapitulatif d'abonnement

--- HAI811I - Developpement mobile/Projet/GPGH - Interimaire/app/src/main/java/com/example/gpgh_interimaire/RecapPaiementActivity.java
+++ HAI811I - Developpement mobile/Projet/GPGH - Interimaire/app/src/main/java/com/example/gpgh_interimaire/RecapPaiementActivity.java
@@ -9,12 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class RecapPaiementActivity extends AppCompatActivity {
 
     TextView typeAboTextView, prixTextView, totalTextView;
-    String typeAbo;
+    boolean is_use = false;
 
     @Override
     @SuppressLint({"MissingInflatedId", "LocalSuppress", "SetTextI18n", "WrongViewCast"})
@@ -25,6 +28,12 @@ public class RecapPaiementActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String typeAbo = intent.getStringExtra("typeAbo");
         int prix = intent.getIntExtra("prix", 0);
+
+        // Rendre invisible les code promo
+        LinearLayout code10 = findViewById(R.id.layout_text_reduction10);
+        code10.setVisibility(View.GONE);
+        LinearLayout code30 = findViewById(R.id.layout_text_reduction30);
+        code30.setVisibility(View.GONE);
 
 
         //modif du texte du paiment de la typeAboTextView et prixTextView
@@ -41,13 +50,50 @@ public class RecapPaiementActivity extends AppCompatActivity {
         totalTextView.setText("Total : "+prix+" €");
 
 
-        ImageView boutonCodePromo = findViewById(R.id.bouton_codePromo);
         EditText codePromo = findViewById(R.id.codePromo);
+        ImageView boutonCodePromo = findViewById(R.id.bouton_codePromo);
         boutonCodePromo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (codePromo.length() == 0){
                     codePromo.setError("Veuillez remplir le champ");
+                }
+                else if (codePromo.getText().toString().equals("CODE202310")){
+                    // Changement du texte du prix total
+                    double prix10 = prix-prix*0.1;
+                    totalTextView.setText("Total : "+prix10+" €");
+                    // Changement de visibilité du layout
+                    LinearLayout code10 = findViewById(R.id.layout_text_reduction10);
+                    code10.setVisibility(View.VISIBLE);
+                    // Changement de prix
+                    TextView prixReduit10 = findViewById(R.id.reductionTextView10);
+                    prixReduit10.setText("- "+prix*0.1+" €");
+                    // Remise à 0 le code promo
+                    codePromo.setText("");
+                    is_use = true;
+
+                }
+                else if (codePromo.getText().toString().equals("CODE202330")){
+                    // Changement du texte du prix total
+                    double prix30 = prix-prix*0.3;
+                    totalTextView.setText("Total : "+prix30+" €");
+                    // Changement de visibilité du layout
+                    LinearLayout code30 = findViewById(R.id.layout_text_reduction30);
+                    code30.setVisibility(View.VISIBLE);
+                    // Changement de prix
+                    TextView prixReduit30 = findViewById(R.id.reductionTextView30);
+                    prixReduit30.setText("- "+prix*0.3+" €");
+                    // Remise à 0 le code promo
+                    codePromo.setText("");
+                    // Dit qu'on est passé par la
+                    is_use = true;
+                }
+                // Verfication pas de cumulable
+                else if (is_use && (codePromo.getText().toString().equals("CODE202330") || codePromo.getText().toString().equals("CODE202310") || codePromo.length() != 0)){
+                    codePromo.setError("Les code promo ne sont pas cumulable");
+                }
+                else {
+                    codePromo.setError("Ce code n'est pas reconnu !");
                 }
                 // Ajouter au prix le code promo
                 // le fait que ya - quelque chose
